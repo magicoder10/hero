fd0774b7e915dac665bf6036fb07ef1382f73508
Harry Liu <byliuyang11@gmail.com>
2018-12-01 23:36:05 -0800

    Add example app

---
+++ src/app/ui.ts
@@ -0,0 +1,6 @@
+import {CalculatorComponent} from './components/calculator.component';
+import {bootstrap} from '@hero';
+
+import globalStyle from './ui.scss';
+
+bootstrap(globalStyle, CalculatorComponent);
